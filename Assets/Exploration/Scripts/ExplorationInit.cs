b1cd78f9ff3ee8c73ce808c0e39265ce5a9bcabc
Mikko Lainio <mikko.lainio@bitbybytecreations.com>
2018-01-27 21:43:22 +0200

    dungu

--- Assets/Exploration/Scripts/ExplorationInit.cs
+++ Assets/Exploration/Scripts/ExplorationInit.cs
@@ -1,19 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class ExplorationInit : MonoBehaviour
 {
+    public int monsterCount;
+    public GameObject playerPrefab;
+    public GameObject[] monsterPrefabs;
+    private LevelBuilder m_builder;
     // Use this for initialization
     void Start()
     {
-        Map map = Loader.LoadFloorTileMap(64);
-        map.GetTiles()[32, 32] = '#';
-        World.Instance.Init(map).Draw();
-
+        //Map map = Loader.LoadFloorTileMap(64);
+        //map.GetTiles()[32, 32] = '#';
+        //World.Instance.Init(map).Draw();
+        m_builder = GetComponent<LevelBuilder>();
+        m_builder.Generate();
+        SpawnMonsters(m_builder.EnemySpawnPoints);
+        SpawnPlayer();
         Invoke("Init", 2f);
     }
 
-    
+    private void SpawnPlayer()
+    {
+        SpawnAtLocation.Spawn(m_builder.StartPoint.X, m_builder.StartPoint.Y, playerPrefab);
+    }
+
+    private void SpawnMonsters(LevelPoint[] enemySpawnPoints)
+    {
+        int count = 0;
+        foreach (LevelPoint point in enemySpawnPoints)
+        {
+            int index = UnityEngine.Random.Range(0, monsterPrefabs.Length);
+            SpawnAtLocation.Spawn(point.X, point.Y, monsterPrefabs[index]);
+            count++;
+            if (count == monsterCount)
+                break;
+        }
+    }
 
     public void Init()
     {
@@ -22,4 +46,5 @@ public class ExplorationInit : MonoBehaviour
         Debug.Log("Synchronizer Create");
         Synchronizer.Start();
     }
+
 }
