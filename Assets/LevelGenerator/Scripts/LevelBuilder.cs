6de978cf758dcb986062cba06f5d836d5ece8529
joni <joni.salmela@bitbybytecreations.com>
2018-01-27 17:43:28 +0200

    now really has levelgenerator

---
+++ Assets/LevelGenerator/Scripts/LevelBuilder.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBuilder : MonoBehaviour {
+
+    private World m_world;
+    // Use this for initialization
+
+    public int LevelWidth = 100;
+    public int LevelHeigth = 80;
+    public int MaxPathLen = 30;
+    public int MinPathLen = 2;
+
+    public int MaxPaths = 10;
+    public int MinPaths = 5;
+
+    void Start()
+    {
+        m_world = World.GetInstance();
+        
+      //  m_world.Draw();
+    }
+    
+    private void fill(char[,] toFill, char with)
+    {
+        for(int i = 0; i < LevelWidth; i++)
+        {
+            for(int o = 0;o< LevelHeigth; o++)
+            {
+                toFill[i, o] = with;
+            }
+        }
+    }
+
+    private LevelPoint[] RunPath(int startX, int startY)
+    {
+        return new LevelPath(startX, startY, MinPathLen, MaxPathLen,LevelWidth,LevelHeigth).RunPath();
+    }
+
+    private void applyPath(char[,] worldData, LevelPoint[] path)
+    {
+        try
+        {
+
+            foreach (LevelPoint point in path)
+            {
+                worldData[point.X, point.Y] = '_';
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    private void GeneratePaths(char[,] worldData)
+    {
+
+        int count = Random.Range(MinPaths,MaxPaths);
+        LevelPoint[] lastPath = RunPath(LevelWidth/2,LevelHeigth/2);
+        applyPath(worldData,lastPath);
+
+        for(int i = 1; i < count; i++)
+        {
+            LevelPoint start = lastPath[Random.Range(0,lastPath.Length)];
+            LevelPoint[] path = RunPath(start.X,start.Y);
+            applyPath(worldData, path);
+            lastPath = path;
+        }
+
+
+    }
+
+    private void purgeOld()
+    {
+
+        if(m_world.GetGrid() == null)
+        {
+            return;
+        }
+
+        foreach(World.Cell cell in m_world.GetGrid().GetCells())
+        {
+            foreach (World.Tile tile in cell.GetTiles())
+            {
+                GameObject.Destroy(tile.GetGO());
+            }
+        }
+    }
+
+    public void Generate()
+    {
+
+        char[,] worldData = new char[LevelWidth,LevelHeigth];
+        fill(worldData,'#');
+
+        purgeOld();
+
+        GeneratePaths(worldData);
+
+        m_world.InitMap(new Map(worldData));
+        m_world.Draw();
+    }
+
+
+}
---
+++ Assets/LevelGenerator/Scripts/LevelPath.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public struct LevelPoint
+{
+    public int X;
+    public int Y;
+}
+
+
+public class LevelPath
+{
+
+    private int m_startX;
+    private int m_startY;
+    private int m_minSize;
+    private int m_maxSize;
+    private int m_levelWith;
+    private int m_levelHeigth;
+
+    private static Random Rand = new Random();
+
+    public LevelPath(int startX, int startY, int minSize, int maxSize, int levelWith, int levelHeigth)
+    {
+        m_startX = startX;
+        m_startY = startY;
+        m_minSize = minSize;
+        m_maxSize = maxSize;
+        m_levelHeigth = levelHeigth;
+        m_levelWith = levelHeigth;
+    }
+
+    private LevelPoint m_direction;
+    private LevelPoint m_lastDirection;
+    private void changeDirection()
+    {
+        m_direction.X = 0;
+        m_direction.Y = 0;
+
+        int dir = Rand.Next(10000) % 4;
+        switch (dir)
+        {
+            case 0:
+                m_direction.X = -1;
+                break;
+            case 1:
+                m_direction.Y = -1;
+                break;
+            case 2:
+                m_direction.X = 1;
+                break;
+            case 3:
+                m_direction.Y = 1;
+                break;
+            case 4:
+                m_direction.X = 1;
+                m_direction.Y = 1;
+                break;
+        }
+
+        if(m_direction.X == m_lastDirection.Y && m_direction.Y == m_lastDirection.Y)
+        {
+            changeDirection();
+        }
+        else
+        {
+            m_lastDirection = m_direction;
+        }
+
+    }
+    public LevelPoint[] RunPath()
+    {
+        changeDirection();
+        int len = Rand.Next(m_minSize, m_maxSize);
+
+        LevelPoint[] path = new LevelPoint[len];
+
+        path[0].X = m_startX;
+        path[0].Y = m_startY;
+        
+        for(int i = 1;i < len; i++)
+        {
+try_again:
+            //should change direction
+            if(Rand.Next(1000) % 3 == 0)
+            {
+                changeDirection();
+            }
+
+
+            path[i].X = path[i - 1].X + m_direction.X;
+            path[i].Y = path[i - 1].Y + m_direction.Y;
+
+            if(path[i].X >= m_levelWith || path[i].Y >= m_levelHeigth || path[i].X < 0 || path[i].Y < 0)
+            {
+                goto try_again;
+            }
+
+        }
+
+        return path;
+    }
+
+}
+
