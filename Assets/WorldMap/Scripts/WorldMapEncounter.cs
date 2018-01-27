c2ffcb6e8eec9a336cd78318cc30957a16e34aab
Petter Skult <petter.skult@gmail.com>
2018-01-27 13:12:22 +0200

    started work on scriptable object encounter

---
+++ Assets/WorldMap/Scripts/WorldMapEncounter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Inventory/List", order = 1)]
+public class WorldMapEncounter : ScriptableObject
+{
+    public string id_ = "New Encounter";
+    public PING_TYPES type_ = PING_TYPES.ENERGY;
+    [Multiline]
+    public string flavorText_;
+}
